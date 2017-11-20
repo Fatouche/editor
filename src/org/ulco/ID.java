528b27fce0994098a4f51056027907c17b6cbf3d
fatouche <foucaud.giovanny@gmail.com>
2017-11-20 10:53:39 +0100

    Question 3+4 Fini

--- src/org/ulco/ID.java
+++ src/org/ulco/ID.java
@@ -1,5 +1,23 @@
 package org.ulco;
 
 public class ID {
-    static public int ID = 0;
+    //static public int ID = 0;
+
+    private static ID INSTANCE = new ID();
+
+    public static ID getInstance(){
+        return INSTANCE;
+    }
+
+    private int id = 0;
+
+    private ID(){}
+
+    public int suivant(){
+        return id=id+1;
+    }
+
+    public int courant(){
+        return id;
+    }
 }
